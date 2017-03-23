d340462c0fbe4303aa382d3903cd54451acd700b
Cheonghyun Lee <lee@cheonghyun.com>
2017-03-24 00:19:05 +0900

    Add Java source codes

---
+++ java/src/main/java/LinearRegression.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Cheong on 3/24/2017.
+ */
+public class LinearRegression {
+    public double slope;
+    public double intercept;
+
+    public static LinearRegression learn(double[] inputs, double[] outputs)
+    {
+        return new LinearRegression();
+    }
+
+    private LinearRegression()
+    {
+
+    }
+
+    /**
+     * input 값에 대한 x 값 계산
+     * @param input 입력값
+     * @return y값
+     */
+    public double transform(double input)
+    {
+        return 0;
+    }
+}
---
+++ java/src/main/java/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Cheong on 3/24/2017.
+ */
+public class Main {
+    /**
+     * dummy main (추후 삭제 예정).
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
