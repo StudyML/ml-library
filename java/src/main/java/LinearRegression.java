b97db20569abec94236d4ca154aca603e62d6a93
Chanjoong <kcj8855@hanmail.net>
2017-04-15 02:14:04 +0900

    최소제곱법,결정계수 추가
    아직 머신러닝기술은 포함 X , 정확도 낮음
    데이터를 가지고 회귀계수를 간단하게 계산하는 방법 2가지만 존재

--- java/src/main/java/LinearRegression.java
+++ java/src/main/java/LinearRegression.java
@@ -4,7 +4,6 @@
 public class LinearRegression {
     private double _slope;
     private double _intercept;
-    private double _residual;
 
     private LinearRegression(double slope, double intercept) {
         _slope = slope;
@@ -12,35 +11,25 @@ public class LinearRegression {
     }
 
     public static LinearRegression learn(double[] inputs, double[] outputs) {
-        //setSlope(10);
-        GraphInfo gInfo = LinearRegression.calcFittedRegressionLine(inputs, outputs);
+        //GraphInfo gInfo = LinearRegression.fittedRegressionLine(inputs, outputs);
+        GraphInfo gInfo = LinearRegression.methodOfLeastSquare(inputs, outputs);
 
         return new LinearRegression(gInfo.slope, gInfo.intercept);
     }
 
-    public void setSlope(double slope) {
-        this._slope = slope;
-    }
 
-    public void setIntercept(double intercept) {
-        this._intercept = intercept;
-    }
-
-    public void setResidual(double residual) {
-        this._residual = residual;
-    }
-
-    private static GraphInfo calcFittedRegressionLine(double[] inputs, double[] outputs) {
-        double xBar = 0;
-        double yBar = 0;
-        double sumXX = 0;
+    //회귀분석의 기본적인 모델(기댓값과 공분산을 이용한 방법)
+    private static GraphInfo fittedRegressionLine(double[] inputs, double[] outputs) {
+        double xBar;
+        double yBar;
+        double sumXX = 0;       //methodOfLeastSquare의 sumXX,sumXY와 다름
         double sumXY = 0;
-        int length = inputs.length;
+        int n = inputs.length;
 
-        xBar = sum(inputs) / length;
-        yBar = sum(outputs) / length;
+        xBar = sum(inputs) / n;            //x의 평균값 E(x)
+        yBar = sum(outputs) / n;           //y의 평균값 E(y)
 
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < n; i++) {
             sumXX += (inputs[i] - xBar) * (inputs[i] - xBar);
             sumXY += (inputs[i] - xBar) * (outputs[i] - yBar);
         }
@@ -52,9 +41,68 @@ public class LinearRegression {
         System.out.println("slope = " + gInfo.slope);
         System.out.println("intercept = " + gInfo.intercept);
 
+        getCoefficientOfDetermination(inputs, outputs); //R-Squared
         return gInfo;
     }
 
+    //최소제곱법을 이용한 회귀모델
+    private static GraphInfo methodOfLeastSquare(double[] inputs, double[] outputs) {
+        double sumX;
+        double sumY;
+        double sumXX = 0;              //clacFittedRegressionLine의 sumXY, sumXX와 다름
+        double sumXY = 0;
+        int n = inputs.length;
+
+        sumX = sum(inputs);
+        sumY = sum(outputs);
+
+        for (int i = 0; i < n; i++) {
+            sumXX += inputs[i] * inputs[i];
+        }
+        for (int i = 0; i < n; i++) {
+            sumXY += inputs[i] * outputs[i];
+        }
+
+        GraphInfo gInfo = new GraphInfo();
+        gInfo.slope = (n * sumXY - sumX * sumY) / (n * sumXX - sumX * sumX);
+        gInfo.intercept = (sumY - gInfo.slope * sumX) / n;
+
+        System.out.println("slope = " + gInfo.slope);
+        System.out.println("intercept = " + gInfo.intercept);
+
+
+        return gInfo;
+    }
+
+    //R-Squared(=결정계수) 계산    0<R-Squared<1 이며 1에 가까울수록 좋음
+    private static double getCoefficientOfDetermination(double[] inputs, double[] outputs){
+        double xBar;
+        double yBar;
+        double sumXY=0;        //methodOfLeastSquare, fittedRegressionLine의 sumXX,sumXY와 다름
+        double sumXX=0;
+        double sumYY=0;
+        int n = inputs.length;
+        double _rSquared;
+
+
+        xBar = sum(inputs) / n;            //x의 평균값 E(x)
+        yBar = sum(outputs) / n;           //y의 평균값 E(y)
+
+        for (int i = 0; i < n; i++) {
+            sumXY += (inputs[i] - xBar) * (outputs[i] - yBar);
+            sumXX += (inputs[i] - xBar) * (inputs[i] - xBar);
+            sumYY += (outputs[i] - yBar) * (outputs[i] - yBar);
+        }
+        sumXY /= n-1;
+        sumXX /= n-1;
+        sumYY /= n-1;
+
+        _rSquared = (sumXY * sumXY) / (sumXX * sumYY);
+
+        System.out.println("_rSquared = " + _rSquared);
+
+        return _rSquared;
+    }
 
     private static double sum(double[] inputs) {
         double sum = 0;
@@ -63,6 +111,7 @@ public class LinearRegression {
         }
         return sum;
     }
+
 
     /**
      * input 값에 대한 x 값 계산
--- java/src/main/java/Main.java
+++ java/src/main/java/Main.java
@@ -9,10 +9,9 @@ public class Main {
      */
     public static void main(String[] args) {
         double[] inputs = {1,2,3,4};
-        double[] outputs = {2,4,6,8};
+        double[] outputs = {3,4,8,9};
 
         LinearRegression model = LinearRegression.learn(inputs, outputs);
-        
-        System.out.println("Hello world");
+        System.out.println(model.transform(5.5));
     }
 }
